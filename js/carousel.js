f781bc82d4011791da6fde1d5572e45a1bb9bd7a
chenweida6220 <weida.wdc@gmail.com>
2022-08-29 00:50:51 -0400

    made some slight changes to z index + added comments

--- js/carousel.js
+++ js/carousel.js
@@ -8,9 +8,12 @@ buttons.forEach(button => {
       .closest("[data-carousel]")
       .querySelector("[data-slides]")
 
+    // Continuous loop of images
     const activeSlide = slides.querySelector("[data-active]")
     let newIndex = [...slides.children].indexOf(activeSlide) + offset
     if (newIndex < 0) newIndex = slides.children.length - 1
+    
+    // Passed the last slide and will loop back to 1st slide
     if (newIndex >= slides.children.length) newIndex = 0
 
     slides.children[newIndex].dataset.active = true
